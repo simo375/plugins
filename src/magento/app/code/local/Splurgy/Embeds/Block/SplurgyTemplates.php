9308f3e1cc51eed0777c38d21afdeafc8b15f354
Doug Mak <doug@splurgy.com>
2012-07-31 17:52:45 -0700

    injected embeds into Checkout, Products Page, and Analytics

---
+++ src/magento/app/code/local/Splurgy/Embeds/Block/SplurgyTemplates.php
@@ -0,0 +1,24 @@
+<?php
+require_once(Mage::getBaseDir('lib') . '/splurgy-lib/SplurgyEmbed.php');
+
+class Splurgy_Embeds_Block_SplurgyTemplates extends Mage_Core_Block_Template
+{	
+	protected $splurgyEmbed;
+
+	public function _construct() {
+		parent::_construct();
+		$this->splurgyEmbed = new SplurgyEmbed;
+
+	}
+    public function getToken() {
+        return $this->splurgyEmbed->getToken();
+    }
+
+    public function getAnalyticsEmbed() {
+    	return $this->splurgyEmbed->getEmbed('analytics')->getTemplate();
+    }
+
+    public function getOffersEmbed() {
+    	return $this->splurgyEmbed->getEmbed('offers', '322')->getTemplate();
+    }
+}
