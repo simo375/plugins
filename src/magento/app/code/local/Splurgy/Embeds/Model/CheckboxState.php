bd7ce2208827672f1031b320201ca6235cd6d405
Splurgy <hai@splurgy.com>
2012-08-01 16:34:22 -0700

    working on wiring the checkbox

---
+++ src/magento/app/code/local/Splurgy/Embeds/Model/CheckboxState.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of CheckboxState
+ *
+ * @author splurgy
+ */
+class CheckboxState
+{
+    //put your code here
+}
+
+?>
