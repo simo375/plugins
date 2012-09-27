9c07f27f41f5992f3a64ee1d7e13d84338f3bd10
Neil Basu <neil@splurgy.org>
2012-09-27 11:52:21 -0700

    Changed magento to write to file again

--- src/lib/SplurgyEmbed.php
+++ src/lib/SplurgyEmbed.php
@@ -21,6 +21,18 @@ class SplurgyEmbed
 
     private $_file;
 
+    public function __construct() {
+        $this->_file = dirname(__FILE__) . '/token.config';
+        // Create a token.config file
+        $this->createTokenConfig();
+    }
+
+    private function createTokenConfig() {
+        if(!file_exists($this->_file)) {
+             file_put_contents($this->_file,'');
+        }
+    }
+    
     public function setToken($token) {
         $token = preg_replace('/[^a-zA-Z0-9_]*/', '', $token);
         $token = str_replace(' ', '', $token);
@@ -31,12 +43,12 @@ class SplurgyEmbed
         if(!preg_match('/^c_[a-zA-Z0-9]{40}$/', $token)) {
             throw new TokenErrorException("Your token is incorrect! Make sure you copied it correctly with no spaces!");
         }
-        update_option('SplurgyToken', $token);
+        file_put_contents($this->_file, $token);
     }
 
     public function getToken() {
-        if (strlen(get_option('SplurgyToken')) > 0) {
-            $token = get_option('SplurgyToken');
+        if(file_exists($this->_file)) {
+            $token = file_get_contents($this->_file);
         }
         return $token;
     }
