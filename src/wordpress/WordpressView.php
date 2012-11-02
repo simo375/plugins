f0dd744f1f754743577b893ec5378fbf7db4b5b0
Doug Mak <doug@splurgy.com>
2012-11-01 18:28:46 -0700

    refactored to reduce cyclomatic complexity for 1 function

--- src/wordpress/WordpressView.php
+++ src/wordpress/WordpressView.php
@@ -3,9 +3,9 @@
 /**
  * All functions for hooks and will output HTML should go here.
  * Wordpress is so dirty, we have to echo!
- * 
+ *
  * PHP version 5.3.1
- * 
+ *
  * @category WordPressView
  * @package  PackageName
  * @author   Splurgy <support@splurgy.com>
@@ -19,7 +19,7 @@ require_once 'splurgy-lib/TemplateGenerator.php';
 
 /**
  * WordPress View Class definition
- * 
+ *
  * @category WordPressView
  * @package  PackageName
  * @author   Splurgy <support@splurgy.com>
@@ -42,21 +42,21 @@ class WordpressView
     public function __construct()
     {
         $this->_splurgyPager = new SplurgyPager();
-        $this->_splurgyEmbed = new SplurgyEmbed(get_option('splurgyToken')); 
+        $this->_splurgyEmbed = new SplurgyEmbed(get_option('splurgyToken'));
         $this->_templateGenerator = new TemplateGenerator();
         $this->_path = dirname(__FILE__). '/view-templates/';
         $this->_templateGenerator->setPath($this->_path);
     }
-    
+
     /**
      * Handles displaying error message
-     * 
+     *
      * @param type $message Message to be displayed
      * @param type $error   Status False/True
-     * 
+     *
      * @return type None
      */
-    public function setWordPressMessage($message, $error=false) 
+    public function setWordPressMessage($message, $error=false)
     {
         if ($error===true) {
             $this->_messages[] = '<div id="message" class="error"><p>'
@@ -66,42 +66,42 @@ class WordpressView
                     .$message.'</p></div>';
         }
     }
-    
+
     /**
      * Handles displaying messages
-     * 
+     *
      * @return type None
      */
-    public function showWordPressMessage() 
+    public function showWordPressMessage()
     {
         foreach ($this->_messages as $message) {
             echo $message;
         }
         $this->_message = null;
     }
-    
+
     /**
      * Notice displayed when Token is not available
-     * 
+     *
      * @return type None
      */
-    public function missingTokenNotice() 
+    public function missingTokenNotice()
     {
         $token = get_option('splurgyToken');
-        if (is_admin() && !isset($token)) {	
+        if (is_admin() && !isset($token)) {
             $url = admin_url('admin.php?page=settings');
             $this->setWordPressMessage(
-                "<b>Splurgy Offers</b> To use this plugin, please configure 
+                "<b>Splurgy Offers</b> To use this plugin, please configure
                 your <a href='$url'>settings</a>", true
-            );	
+            );
         }
     }
-    
+
     /**
      * If the Splurgy Offers checkbox is ON on the WordPress Pages this function
      * is called [This is deprecated after the introduction of short codes since
      * we will not make Splurgy ON/OFF checkbox available on Posts
-     * 
+     *
      * @return type None
      */
 
@@ -136,11 +136,11 @@ class WordpressView
         );
         echo $this->_templateGenerator->getTemplate();
     }
-    
+
     /**
-     * If the Splurgy Offers checkbox is ON on the WordPress Pages this function 
+     * If the Splurgy Offers checkbox is ON on the WordPress Pages this function
      * is called
-     * 
+     *
      * @return type None
      */
     public function pageMetaBoxOfferList()
@@ -160,40 +160,40 @@ class WordpressView
             $checked = "checked='checked'";
             $showOfferId = "style='display: inline;'";
         }
-        
+
         if ('true' == $TestMode[0]) {
             $testchecked = 'checked=checked';
         }
         if ('false' == $TestMode[0]) {
             $testchecked = '';
         }
-        
+
         if (!empty($unlocktext[0]) && $unlocktext[0] != 'true') {
             $unlocktextinput = $unlocktext[0];
         }
-        
+
         $currentOfferId =  "Default Offer is set";
         if (!empty($splurgyOfferId)) {
             $offerId = $splurgyOfferId[0];
             $currentOfferId =  "Current showing offer #: <b>" .$offerId. "</b>";
-        } 
+        }
 
         $this->_templateGenerator->setTemplateName('pageMetaBoxOfferList');
         $this->_templateGenerator->setPatterns(
-            array('{$checked}', '{$testchecked}', '{$showOfferId}', 
+            array('{$checked}', '{$testchecked}', '{$showOfferId}',
                 '{$currentOfferId}', '{$unlocktextinput}')
         );
         $this->_templateGenerator->setReplacements(
-            array($checked, $testchecked, $showOfferId, $currentOfferId, 
+            array($checked, $testchecked, $showOfferId, $currentOfferId,
                 $unlocktextinput)
         );
         echo $this->_templateGenerator->getTemplate();
     }
-    
-    
+
+
     /**
      * Displays the content for the Short Code Help from the templates
-     * 
+     *
      * @return type None
      */
     public function pagePostMetaBoxShortCodeHelp()
@@ -201,36 +201,36 @@ class WordpressView
         $this->_templateGenerator->setTemplateName('pagePostMetaBoxShortCodeHelp');
         echo $this->_templateGenerator->getTemplate();
     }
-    
+
     /**
      * Displays the content for the Settings Page from the templates
-     * 
-     * @return type None
-     */
-            
-    public function settingsPage()
-    {
-        $token = $this->_splurgyEmbed->getToken(); 
-        $this->settingsPageView($token);
-    }
-    
-    /**
-     * This will set the Settings Page View html
-     * 
-     * @param type $token The Token for the current channel
-     * 
+     *
      * @return type None
      */
 
-    public function settingsPageView($token) 
+    public function settingsPage()
+    {
+        $token = $this->_splurgyEmbed->getToken();
+        $this->settingsPageView($token);
+    }
+
+    /**
+     * This will set the Settings Page View html
+     *
+     * @param type $token The Token for the current channel
+     *
+     * @return type None
+     */
+
+    public function settingsPageView($token)
     {
         echo "<h2>Settings</h2>";
         $message = '';
         $previewAndReset = '';
-        
+
         if (!empty($token)) {
             $message = "Your current token is <b>$token</b><br/>";
-            $message .= "You now have options to add offers when adding a 
+            $message .= "You now have options to add offers when adding a
                 new post!<br/>";
         } else {
             $message = "Your token is not setup right now<br/><br/>";
@@ -259,10 +259,10 @@ class WordpressView
         );
         echo $this->_templateGenerator->getTemplate();
     }
-    
+
     /**
      * Handler for the Settings Page [checks if the token is available]
-     * 
+     *
      * @return type None
      */
 
@@ -281,10 +281,10 @@ class WordpressView
             $this->_splurgyEmbed->deleteToken();
         }
     }
-    
+
     /**
      * Analytics Page Function
-     * 
+     *
      * @return type None
      */
 
@@ -296,28 +296,28 @@ class WordpressView
         $this->_templateGenerator->setReplacements($img);
         echo $this->_templateGenerator->getTemplate();
     }
-    
+
     /**
      * Short Code Function that makes the splurgy offer available via short code
-     * 
+     *
      * @param type $atts Attributes OfferId and TestMode for the Shortcode
-     * 
+     *
      * @return type the do_shortcode content i.e. the deal from Splurgy
      */
-    
-    public function splurgyShortCode($atts) 
+
+    public function splurgyShortCode($atts)
     {
         extract(
             shortcode_atts(
                 array(
                     'offerid' => null,
                     'testmode' => false,
-                ), 
+                ),
                 $atts
-            )   
+            )
         );
-        
-        
+
+
         if ($offerid != null && !is_page()) {
             /** Offer Id specified and this is a post 1st IF**/
             return do_shortcode(
@@ -335,16 +335,16 @@ class WordpressView
             );
         }
     }
-    
+
     /**
      * Main Function that will display the offer/content-lock on the Page / Post
-     * 
+     *
      * @param type $content The content
-     * 
+     *
      * @return type None
      */
 
-    public function offer($content) 
+    public function offer($content)
     {
         //TODO: remname since both offer and content-lock can be created here
         /**echo $content;**/
@@ -353,7 +353,7 @@ class WordpressView
         $testmodevalue = get_post_custom_values('TestMode');
         $unlocktextvalue = get_post_custom_values('unlocktext');
         $sOfferPowerSwState = get_post_custom_values('SplurgyOfferPowerSwitch');
-        if ('off' != $sOfferPowerSwState[0] && null != $sOfferPowerSwState[0]) {
+        if ('off' != $sOfferPowerSwState[0] && !is_null($sOfferPowerSwState[0])) {
             if (!empty($splurgyOfferId) && !is_page()) {
                 $offerId = $splurgyOfferId[0];
                 if ((is_single() || $this->_offerCount < 3)) {
@@ -362,17 +362,15 @@ class WordpressView
                         ->getTemplate();
                     $this->_offerCount++;
                 } else {
-                    //$permalink = '" . get_permalink() . "#SplurgyOffer';
+                    $permalink = get_permalink() . '#SplurgyOffer';
                     $this->_templateGenerator->setTemplateName('offer');
                     $this->_templateGenerator->setPatterns('{$permalink}');
-                    $this->_templateGenerator->setReplacements(
-                        "" . get_permalink() . "#SplurgyOffer"
-                    );
+                    $this->_templateGenerator->setReplacements($permalink);
                     echo $this->_templateGenerator->getTemplate();
                 }
             } elseif (is_page() && !empty($splurgyOfferId)) {
                 if (is_page() && !empty($splurgyOfferId)) {
-                    // TODO: make this dynamic based on type 
+                    // TODO: make this dynamic based on type
                     // ('page-offer' or 'content-lock')
                     $offerId = $splurgyOfferId[0];
                     $testmode = $testmodevalue[0];
@@ -381,16 +379,16 @@ class WordpressView
                         'content-lock', $offerId, $testmode, $unlocktext
                     )->getTemplate(); // 'page-offer'
                 }
-            } 
+            }
             /**else {
                 echo $this->_splurgyEmbed->getEmbed('page-offer')->getTemplate();
             }**/
         }
     }
-    
+
     /**
      * Embeds Analytics
-     * 
+     *
      * @return type None
      */
 
@@ -401,7 +399,7 @@ class WordpressView
 
     /**
      * Adds a box to the main column on the Post and Page edit screens
-     * 
+     *
      * @return type None
      */
 
@@ -409,7 +407,7 @@ class WordpressView
     {
         /** Removing this since we want only short codes working on the Posts
          * add_meta_box(
-         *   'myplugin_sectionid', __('Splurgy Offers!', 'myplugin_textdomain'), 
+         *   'myplugin_sectionid', __('Splurgy Offers!', 'myplugin_textdomain'),
          * array($this, 'postMetaBoxOfferList'), 'post', 'side', 'high'
         );**/
 
@@ -417,29 +415,29 @@ class WordpressView
             'myplugin_sectionid', __('Splurgy Page Lock', 'myplugin_textdomain'),
             array($this, 'pageMetaBoxOfferList'), 'page', 'side', 'high'
         );
-        
+
         add_meta_box(
             'myplugin_sc_sectionid', __(
                 'Splurgy Short Code Help', 'myplugin_sc_textdomain'
             ), array($this, 'pagePostMetaBoxShortCodeHelp'), 'page', 'normal', 'high'
         );
-        
+
         add_meta_box(
             'myplugin_sc_sectionid', __(
                 'Splurgy Short Code Help', 'myplugin_sc_textdomain'
             ), array($this, 'pagePostMetaBoxShortCodeHelp'), 'post', 'normal', 'high'
-        );        
+        );
     }
 
     /**
-     * Save Offers Meta Box Data on Posts 
-     * 
+     * Save Offers Meta Box Data on Posts
+     *
      * @param type $post_id Id of the Post
-     * 
+     *
      * @return type None
      */
     public function savePostMetaBoxOfferData($post_id)
-    { 
+    {
         if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
             return;
         }
@@ -448,67 +446,117 @@ class WordpressView
          * Verify this came from the our screen and with proper authorization,
          * because save_post can be triggered at other times
          */
-        
-        $authCheckForPost = wp_verify_nonce(
+
+        $authCheckBeforeSave = wp_verify_nonce(
             $_POST['splurgyOfferNonce'], plugin_basename(__FILE__)
         );
-        if (!$authCheckForPost) {
-            return;
-        }
-        /** Check permissions **/
 
-        if (!current_user_can('edit_post', $post_id)) {
+        /** Check permissions **/
+        if (!$authCheckBeforeSave || !current_user_can('edit_post', $post_id)) {
             return;
         }
-        
-        $offerPowerSwState = $_POST['offerPowerSwitch'];
-        if (is_null($offerPowerSwState)) {
+
+        $this->_insertPowerSwitch($post_id, $_POST['offerPowerSwitch']);
+        $this->_insertTestMode($post_id, $_POST['testmode']);
+        $this->_insertPageLock($post_id, $_POST['pagelocktext']);
+        $this->_insertOfferId($post_id, $_POST['offerId']);
+    }
+
+    /**
+     * Inserts power switch data into database
+     *
+     * @param type $post_id           Post ID
+     * @param type $offerPowerSwState Post Data from Page save
+     *
+     * @return type None
+     */
+
+    private function _insertPowerSwitch($post_id, $offerPowerSwState)
+    {
+
+        if (!is_null($offerPowerSwState)) {
+            $offerPowerSwState = 'on';
+        } else {
             $offerPowerSwState = 'off';
         }
+
         add_post_meta(
             $post_id, 'SplurgyOfferPowerSwitch', $offerPowerSwState, true
         ) or update_post_meta(
             $post_id, 'SplurgyOfferPowerSwitch', $offerPowerSwState
         );
-        
-        $testmode = $_POST['testmode'];
-        if (isset($_POST['testmode'])) {
-            $testmode = 'true';
-        } else {
-            $testmode = 'false';
-        }
-        ;
-
-        add_post_meta($post_id, 'TestMode', $testmode, true) or 
-                update_post_meta($post_id, 'TestMode', $testmode);
-        
-        $unlocktext = $_POST['pagelocktext'];
-        
-        if (empty($_POST['pagelocktext'])) {
-            $unlocktext= 'true';
-        }
-        ;
-        
-        add_post_meta($post_id, 'unlocktext', $unlocktext, true) or 
-                update_post_meta($post_id, 'unlocktext', $unlocktext);
-
-        
-        $offerId = intval(trim($_POST['offerId']));
-        if ( 0 >= $offerId ) {
-            return;
-        }
-
-        add_post_meta($post_id, 'SplurgyOfferId', $offerId, true) or 
-                update_post_meta($post_id, 'SplurgyOfferId', $offerId);
-
     }
-    
-    /** 
-     * Add Buttons On Init
-     * 
+
+
+    /**
+     * Inserts test mode data into database
+     *
+     * @param type $post_id  Post ID
+     * @param type $testMode Post Data from Page save
+     *
      * @return type None
      */
-    
+
+    private function _insertTestMode($post_id, $testMode)
+    {
+        switch($testMode) {
+        case 'on':
+            $testMode = 'true';
+            break;
+        default:
+            $testMode = 'false';
+            break;
+        }
+        var_dump($testMode);
+        add_post_meta($post_id, 'TestMode', $testMode, true) or
+            update_post_meta($post_id, 'TestMode', $testMode);
+
+    }
+
+    /**
+     * Inserts unlock text data into database
+     *
+     * @param type $post_id    Post ID
+     * @param type $unlocktext Post Data from Page save
+     *
+     * @return type None
+     */
+
+    private function _insertPageLock($post_id, $unlocktext)
+    {
+        if (!empty($unlocktext)) {
+            add_post_meta($post_id, 'unlocktext', $unlocktext, true) or
+                update_post_meta($post_id, 'unlocktext', $unlocktext);
+        }
+
+    }
+
+    /**
+     * Inserts offer id data into database
+     *
+     * @param type $post_id Post ID
+     * @param type $offerId Post Data from Page save
+     *
+     * @return type None
+     */
+
+    private function _insertOfferId($post_id, $offerId)
+    {
+
+        if ( 0 < intval(trim($offerId)) ) {
+            add_post_meta($post_id, 'SplurgyOfferId', $offerId, true) or
+                update_post_meta($post_id, 'SplurgyOfferId', $offerId);
+        }
+    }
+
+
+
+    /**
+     * Add Buttons On Init
+     *
+     * @return type None
+     */
+
     public function addButtonsOnInit()
     {
         add_filter('mce_buttons', array($this, 'addButtonToPost'));
