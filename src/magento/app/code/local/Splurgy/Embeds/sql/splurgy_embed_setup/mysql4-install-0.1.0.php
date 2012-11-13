bedc046d46d89c9a8d3534e894e17a68b70b0cda
Neil Basu <neil@splurgy.org>
2012-11-13 13:32:28 -0800

    Deprecated old admin menu items

--- src/magento/app/code/local/Splurgy/Embeds/sql/splurgy_embed_setup/mysql4-install-0.1.0.php
+++ src/magento/app/code/local/Splurgy/Embeds/sql/splurgy_embed_setup/mysql4-install-0.1.0.php
@@ -3,19 +3,7 @@
 $installer = $this;
  
 $installer->startSetup();
- /*
-$installer->run("
- 
--- DROP TABLE IF EXISTS {$this->getTable('splurgy_embed')};
-CREATE TABLE {$this->getTable('splurgy_embed')} (
-  `splurgy_embed_id` int(11) unsigned NOT NULL auto_increment,
-  `entityid` int(11) unsigned UNIQUE NULL,
-  `status` smallint(6) NOT NULL default '0',
-  `offerid` int(11) unsigned NULL,
-  PRIMARY KEY (`splurgy_embed_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-");
-       */         
+     
 $installer->run("                
 -- DROP TABLE IF EXISTS {$this->getTable('splurgy_banner')};
 CREATE TABLE {$this->getTable('splurgy_banner')} (
