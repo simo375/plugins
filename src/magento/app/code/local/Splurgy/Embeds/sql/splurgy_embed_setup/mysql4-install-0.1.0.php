1aff48e33953f0492b13e0b1df17aa94280f56ec
Neil Basu <neil@splurgy.org>
2012-11-13 16:37:11 -0800

    Installation error that I fixed, updated how-to

--- src/magento/app/code/local/Splurgy/Embeds/sql/splurgy_embed_setup/mysql4-install-0.1.0.php
+++ src/magento/app/code/local/Splurgy/Embeds/sql/splurgy_embed_setup/mysql4-install-0.1.0.php
@@ -7,7 +7,7 @@ $installer->startSetup();
 $installer->run(
     "-- DROP TABLE IF EXISTS {$this->getTable('splurgy_banner')};
 CREATE TABLE {$this->getTable('splurgy_banner')} (
-  `splurgy_banner_id` int(11) unsigned NOT NULL auto_increment,
+  `splurgy_banner_id` int(11) unsigned NOT NULL,
   `entityid` int(11) unsigned UNIQUE NOT NULL auto_increment,
   `status` smallint(6) NULL default '0',
   `offerid` int(11) unsigned NULL,
