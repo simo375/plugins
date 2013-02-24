2654f4299f131105a0b8ea4b45d19b9d7ce70e29
Doug Mak <doug@splurgy.com>
2013-02-23 17:08:39 -0800

    added symlink for splurgy-publisher and added empty class for embed generating

---
+++ src/lib/SplurgyEmbedGenerator.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * This is for Simon to implement
+ */
+require_once 'Exceptions.php';
+require_once 'TemplateGenerator.php';
+
+class SplurgyEmbedGenerator extends TemplateGenerator
+{
+
+}
+
+?>
--- src/splurgy-publisher/splurgy-lib/Exceptions.php
+++
@@ -1,4 +0,0 @@
-<?php
-class TokenErrorException extends Exception {}
-class TemplateErrorException extends Exception {}
-?>
